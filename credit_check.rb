1115f4ca7c143ff7de334b8d3f1d16019cdcc37d
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-06-27 18:49:20 -0600

    First_draft

--- credit_check.rb
+++ credit_check.rb
@@ -1,41 +1,33 @@
-puts "Enter your 16 digit card number"
-card_number = gets.chomp.to_s
-#test it
-puts card_number
-#reverse your string
-card_number.reverse!
-#test it
-puts card_number
-#do i even need this thing? it will take all the numbers and split them up
-#buuuut...uhhh...how do i integrate that?
-individual_numbers = card_number.split('')
-#ENUMBERABLE GOES HERE. OTHERWISE DO THE HARD LEGWORK WRITTEN BELOW.
-#go to the second value in the string.
-second_number = card_number[1]
-#test it
-puts second_number
-#do so until the end of the array
-fourth_number = card_number[3]
-#and continue to test
-puts fourth_number
-sixth_number = card_number[5]
-puts sixth_number
-eighth_number = card_number[7]
-puts eighth_number
-tenth_number = card_number[9]
-puts tenth_number
-twelfth_number = card_number[11]
-puts twelfth_number
-fourteenth_number = card_number[13]
-puts fourteenth_number
-sixteenth_number = card_number[15]
-puts sixteenth_number
-# ok times each one of those by two
+card_number_input = "6011797668868728"
 
-# compare the product of those calculation to the number 9.
-# if the product of the calculation is greater than nine, add the two integers that make up the number
-# and put that into the string in place of the origional product
-# add all the digits together
-# divide the product by 10
-# if the modulo is 0, print "valid"
-# if the module is anything but 0, print "invalid"
+split_up_number = card_number_input.split('').reverse
+test_array = []
+split_up_number.each do |number|
+  test_array << number.to_i
+end
+
+doubles = []
+test_array.each.with_index do |number, index|
+  if index.odd?
+    doubles << number * 2
+  else doubles << number
+  end
+end
+
+array_to_be_added_up = []
+doubles.each do |number|
+  if number > 9
+    array_to_be_added_up << number -9
+  else array_to_be_added_up << number
+  end
+end
+
+final_number = array_to_be_added_up.sum
+puts final_number
+
+result = final_number % 10
+puts result
+if result == 0
+  puts "Valid"
+else puts "Invalid"
+end
